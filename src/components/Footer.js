62e8988f118d613144ebc2d613377ec624f1f8a5
Youri-Ikhelef <ikhelef.y@gmail.com>
2024-03-13 21:05:13 +0100

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <>
-      <p className="russo">Site réalisé par Youri IKHELEF</p>
-    </>
-  );
-};
-
-export default Footer;
--- src/store/MorpionContext.js
+++
@@ -1,7 +0,0 @@
-import { createContext, useContext } from "react";
-
-export const MorpionContext = createContext();
-
-export const useMorpionContext = () => {
-  return useContext(MorpionContext);
-};
